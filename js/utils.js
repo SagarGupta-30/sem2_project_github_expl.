bf4a46d3ea1d78e8d9eb45ab777f6d68cbd85943
mdshahnawazshamim <shahnawaz.shamim@outlook.com>
2026-06-27 19:48:11 +0530

    Implement getUniqueLanguages function
    Added a function to retrieve unique programming languages from repositories.

--- js/utils.js
+++ js/utils.js
@@ -55,7 +55,34 @@
 //=================================================//
 
 
-//sahanawaz//
+//shahnawaz//
+
+/*
+Unique Languages
+*/
+
+function getUniqueLanguages(repos){
+const languages =
+repos
+.map(
+)
+repo => repo.language
+.filter(Boolean);
+return [...new Set(languages)];
+}
+
+/*
+Global Object
+*/
+
+window.Utils = {
+sortRepositories,
+filterRepositories,
+formatDate,
+formatNumber,
+getUniqueLanguages
+};
+})();
 
 
 
