ac444ad8529fd34ed29d315a83408802cef307a2
SagarGupta-30 <guptasagar0130@gmail.com>
2026-06-27 15:45:53 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,240 @@
+// Anshu negi //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------//
+
+
+//utkarsh verma //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------//
+
+//sagar gupta//
+/*
+ Search Form
+*/
+
+searchForm.addEventListener(
+    "submit",
+    handleSearch
+);
+
+async function handleSearch(event){
+
+    event.preventDefault();
+
+    const username =
+        searchInput.value.trim();
+
+    if(!username){
+
+        UI.showSearchError(
+            "Please enter username"
+        );
+
+        return;
+    }
+
+    UI.hideSearchError();
+
+    currentUsername = username;
+
+    await loadUser(username);
+
+}
+
+
+/*
+ Load User
+*/
+
+async function loadUser(username){
+
+    UI.hideError();
+
+    UI.showLoader();
+
+    try{
+
+        const data =
+            await API.fetchUserAndRepos(
+                username
+            );
+
+        const user =
+            data.user;
+
+        const repos =
+            data.repos;
+
+        allRepos = repos;
+
+        const languages =
+            Utils.getUniqueLanguages(
+                repos
+            );
+
+        UI.populateLangFilter(
+            languages
+        );
+
+        UI.renderProfile(
+            user
+        );
+
+        UI.renderRepositories(
+            repos
+        );
+
+        UI.showResults();
+
+    }
+
+    catch(error){
+
+        UI.showError(
+            "Error",
+            error.message
+        );
+
+    }
+
+    finally{
+
+        UI.hideLoader();
+
+    }
+
+}
+
+
+/*
+ Retry Button
+*/
+
+retryBtn.addEventListener(
+    "click",
+    ()=>{
+
+        if(currentUsername){
+
+            loadUser(
+                currentUsername
+            );
+
+        }
+
+    }
+);
+
+
+/*
+ Repository Search
+*/
+
+repoSearch.addEventListener(
+    "input",
+    filterAndSortRepos
+);
+
+
+/*
+ Language Filter
+*/
+
+langFilter.addEventListener(
+    "change",
+    filterAndSortRepos
+);
+
+
+/*
+ Sort
+*/
+
+sortSelect.addEventListener(
+    "change",
+    filterAndSortRepos
+);
+//---------------------------------------------------------------//
+
+// sahnawaz //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//========================end================================//
+
